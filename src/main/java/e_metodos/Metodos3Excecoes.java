f4286fec9bafafc1adcc3867c33b32024c27e086
handrade <hugo.andrade@selic.anbima.com.br>
2025-11-30 21:06:02 -0300

    feat: atualizando package metodos

--- src/main/java/e_metodos/Metodos3Excecoes.java
+++ src/main/java/e_metodos/Metodos3Excecoes.java
@@ -4,37 +4,47 @@ public class Metodos3Excecoes {
 
     public static void main(String[] args) throws Exception {
 
-        divisaoFixa();
-
-        conversaoIdade("25");
-
-        conversaoIdade("vinte");
-
-        String texto = pegarLetra("abc", 5);
+//        lancaExcecao();
+//
+//        String texto = pegarLetra("abc", 5);
+//        System.out.println("Letra: " + texto + "\n");
+//
+//
+//        divisaoFixa();
+//
+//        conversaoIdade("25");
+//
+//        conversaoIdade("vinte");
 
     }
 
-    public static void divisaoFixa() {
-        try {
-            int resultado = 10 / 0;
-            System.out.println("Resultado: " + resultado);
-        } catch (ArithmeticException e) {
-            System.out.println("Erro: divisão por zero!\n\n");
-        }
-    }
 
-    public static void conversaoIdade(String entrada) {
-        try {
-            int idade = Integer.parseInt(entrada);
-            System.out.println("Sua idade é: " + idade + " anos.\n");
-        } catch (NumberFormatException e) {
-            System.out.println("Por favor, digite um número válido para a idade.\n");
-        } finally {
-            System.out.println("Processo de leitura de idade finalizado.\n");
-        }
-    }
-
-    public static String pegarLetra(String texto, int indice) throws Exception {
-        return String.valueOf(texto.charAt(indice));
-    }
+//    public static void lancaExcecao() throws Exception {
+//        System.out.println("Apenas para lançar uma exceção.\n");
+//        throw new Exception("Exceção criada para estudo de caso!");
+//    }
+//
+//    public static String pegarLetra(String texto, int indice) throws Exception {
+//        return String.valueOf(texto.charAt(indice));
+//    }
+//
+//    public static void divisaoFixa() {
+//        try {
+//            int resultado = 10 / 0;
+//            System.out.println("Resultado: " + resultado);
+//        } catch (ArithmeticException e) {
+//            System.out.println("Erro: divisão por zero!\n\n");
+//        }
+//    }
+//
+//    public static void conversaoIdade(String entrada) {
+//        try {
+//            int idade = Integer.parseInt(entrada);
+//            System.out.println("Sua idade é: " + idade + " anos.\n");
+//        } catch (NumberFormatException e) {
+//            System.out.println("Por favor, digite um número válido para a idade.\n");
+//        } finally {
+//            System.out.println("Processo de leitura de idade finalizado.\n");
+//        }
+//    }
 }
