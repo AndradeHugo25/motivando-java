3b802f2084dc15568f3b50e82a646430f7d11e67
Eduardo Barros <eduardibarros@gmail.com>
2026-01-22 19:29:11 -0300

    1 commit

--- src/main/java/a_basico/Basico1Comentarios.java
+++ src/main/java/a_basico/Basico1Comentarios.java
@@ -4,7 +4,7 @@ public class Basico1Comentarios {
 
     public static void main(String[] args) {
         // comentários são ignorados pelo compilador
-
+        // comentando só para commitar
         // atalho: ctrl + /
 
         // atalho comentário em bloco: ctrl + shift + /
