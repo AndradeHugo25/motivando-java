17428d011a66dfe02c9baa6cbe14f2390a0a330d
handrade <hugo.andrade@selic.anbima.com.br>
2026-01-30 16:00:17 -0300

    feat: melhoria da correção tópico 7

--- src/main/java/h_bd/exercicios/Main.java
+++ src/main/java/h_bd/exercicios/Main.java
@@ -10,37 +10,34 @@ import java.util.Map;
 public class Main {
 
     public static void main(String[] args) throws Exception {
-
         ArtistaRepository artistaRepository = new ArtistaRepository();
         String nomeArtista = artistaRepository.selecionarNomeArtistaPorId(3);
 
         AlbumRepository albumRepository = new AlbumRepository();
         List<Map<String, Object>> album = albumRepository.selecionarAlbumPorArtistaId(3);
 
-        System.out.println("\nO artista é: " + nomeArtista + "\n");
-        System.out.println("Albuns do artista:" + nomeArtista + "\n");
+        System.out.println("\nÁlbuns do artista: " + nomeArtista);
         for (Map<String, Object> albums : album) {
-            System.out.println(albums.get("nome"));
+            System.out.println("\t" + albums.get("nome"));
         }
 
+        System.out.println();
+
         PlaylistRepository playlistRepository = new PlaylistRepository();
+        System.out.println("\nPlaylist original: " + playlistRepository.selecionarPlaylist(3));
+
+        String novaPlaylist = gerarNovaPlaylist(playlistRepository);
+        playlistRepository.atualizarPlaylist("id", 3, "nome", novaPlaylist);
+        System.out.println("\nPlaylist alterada: " + playlistRepository.selecionarPlaylist(3));
+    }
+
+    private static String gerarNovaPlaylist(PlaylistRepository playlistRepository) {
         String playlistAtual = playlistRepository.selecionarPlaylist(3);
-        System.out.println(playlistAtual);
-
         String[] splitado = playlistAtual.split(" ");
-        int valorIndex = Integer.parseInt(splitado[2]);
-        System.out.println(valorIndex);
 
+        int valorIndex = Integer.parseInt(splitado[2]);
         int novoIndex = valorIndex + 1;
 
-        playlistAtual = playlistAtual.replace(String.valueOf(valorIndex), String.valueOf(novoIndex));
-        System.out.println(playlistAtual);
-
-        playlistRepository.atualizarPlaylist("id", 3, "nome", playlistAtual);
-
-//        int playlistAlterada = playlistRepository.atualizarPlaylist("id", 3, "nome", "Minha playlist 1");
-//        String nomePlaylist = (playlistRepository.selecionarPlaylist(3));
-//
-        System.out.println("Playlist alterada: " + playlistAtual);
+        return playlistAtual.replace(String.valueOf(valorIndex), String.valueOf(novoIndex));
     }
 }
