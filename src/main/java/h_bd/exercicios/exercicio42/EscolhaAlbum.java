21d8ae2a400f5fac52758cb659ad77b873e4ef7c
emmanuel <pedro.oliveira@selic.anbima.com.br>
2026-02-19 21:15:35 -0300

    feat: exercícios do oitavo módulo. Ajustado o exercicio 42. obs: eu mantive o errado para depois dar uma estudada.

--- src/main/java/h_bd/exercicios/exercicio42/EscolhaAlbum.java
+++ src/main/java/h_bd/exercicios/exercicio42/EscolhaAlbum.java
@@ -2,16 +2,20 @@ package h_bd.exercicios.exercicio42;
 
 import h_bd.repository.AlbumRepository;
 import java.util.List;
+import java.util.Map;
 
 public class EscolhaAlbum {
     public static void main(String[] args){
         AlbumRepository albumRepository = new AlbumRepository();
-        int artistaId = 4;
-        List<String> nomesAlbuns = albumRepository.selecionarNomesAlbunsPorArtistaId(artistaId);
+        int artistaId = 3;
+        List<Map<String, Object>> albuns = albumRepository.selecionarNomesAlbunsPorArtistaId(artistaId);
 
         System.out.println("Álbuns do artista com id " + artistaId + ":");
-        for (String nomeAlbum : nomesAlbuns) {
-            System.out.println(nomeAlbum);
+        for (Map<String, Object> album : albuns) {
+            Object nome = album.get("nome");
+            if (nome != null) {
+                System.out.println(nome.toString());
+            }
         }
     }
 }
--- src/main/java/h_bd/repository/AlbumRepository.java
+++ src/main/java/h_bd/repository/AlbumRepository.java
@@ -1,6 +1,7 @@
 package h_bd.repository;
 
 import h_bd.database.DBConnection;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -8,17 +9,16 @@ import java.util.Map;
 public class AlbumRepository {
 
     private final DBConnection dbConnection;
+    private final String NOME_TABELA = "album";
 
     public AlbumRepository() {
         this.dbConnection = new DBConnection();
         this.dbConnection.connectSQLiteDB("src/main/resources/auto-qa.db");
     }
-    public List<String> selecionarNomesAlbunsPorArtistaId(int artistaId) {
-        List<Map<String, Object>> resultado = dbConnection.select("album", "artista_id", artistaId);
-        List<String> nomesAlbuns = new java.util.ArrayList<>();
-        for (Map<String, Object> linha : resultado) {
-            nomesAlbuns.add(linha.get("nome").toString());
-        }
-        return nomesAlbuns;
+
+    public List<Map<String, Object>> selecionarNomesAlbunsPorArtistaId(int artistaId) {
+        return dbConnection.select(NOME_TABELA, "artista_id", artistaId);
+
+
     }
 }
